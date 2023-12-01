3afbb559e155a6087e4fa2fa8447f176b5717773
pencilCool <yhtangcoder@gmail.com>
2023-12-01 08:00:23 +0800

    Initial Commit

---
+++ DoubleCalc/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DoubleCalc
+//
+//  Created by yuhua Tang on 2023/12/1.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
